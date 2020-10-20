20cfd15269cbdc318fdc23ec55b45fb6dec57660
Katie Wall <katie.wall@richmond.edu>
2020-10-19 23:16:35 -0400

    added Shape.cpp

---
+++ Shape.cpp
@@ -0,0 +1,32 @@
+#ifndef __SHAPE_CPP__
+#define __SHAPE_CPP__
+
+#include <iostream>
+#include "Shape.h"
+
+using namespace std;
+
+//constructor
+Shape::Shape(string shapeName) : name{shapeName} {
+	cout << "In Shape typical-use constructor" << endl;
+}
+
+//deconstructor
+Shape::~Shape() {}
+
+double Shape::getArea(){
+	return name.getArea();
+}
+
+void Shape::print(){
+	cout << "In the Shape class's print statement" << endl;
+	cout << "name = " << this->name << endl;
+}
+
+/*virtual void print(){
+
+}
+
+virtual void print() = 0{
+	
+}*/
